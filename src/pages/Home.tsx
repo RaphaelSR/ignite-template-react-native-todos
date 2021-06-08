fd6c353e86e28de0dd23e19d03e8bc076c0e5319
Raphael Souza Rocha <raphaelrochabcc@gmail.com>
2021-06-08 17:13:02 -0300

    Done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,17 +11,37 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
+
+    if (newTaskTitle){
+      setTasks(oldState => [
+        {
+          id: new Date().getTime(),
+          title: newTaskTitle,
+          done: false,
+
+      }, ...oldState])
+    }
+
     //TODO - add new task if it's not empty
   }
 
   function handleMarkTaskAsDone(id: number) {
+
+    const task = tasks.filter((item) => item.id === id)[0]
+
+    task.done = !task.done
+
+    const newTasks = [...new Set([task, ...tasks])];
+    setTasks(newTasks)
+    
     //TODO - mark task as done if exists
   }
 
   function handleRemoveTask(id: number) {
+    setTasks(oldState => oldState.filter(task => task.id !== id) )
     //TODO - remove task from state
   }
 
